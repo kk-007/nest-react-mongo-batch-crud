1b9e6dac7e6d605bacecef34a63079f097ce548b
keval navadiya <kevalnavadiya39@gmail.com>
2023-08-16 09:54:20 +0530

    connection between frontend and backend app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.text())
+      .then(setResponse);
+  }, []);
 
   return (
     <>
@@ -25,11 +32,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">{response}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
